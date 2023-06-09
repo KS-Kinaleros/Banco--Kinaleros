3bfd4f592c155d0f3b11359cdb758c18bf9539fa
dvelasquez <dvelasquez-2021489@kinal.edu.gt>
2023-06-09 03:51:42 -0600

    Se copio Back y Front de la rama morellana

---
+++ Banco-Back-End/configs/app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+// rutas
+const userRoutes = require('../src/user/user.routes')
+
+const app = express()
+const port = process.env.PORT || 3100
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors())
+app.use(helmet())
+app.use(morgan('dev'))
+//para usar las rutas
+app.use('/user', userRoutes )
+
+exports.initServer = () =>{
+    app.listen(port)
+    console.log(`Server is running in port ${port}`)
+}
