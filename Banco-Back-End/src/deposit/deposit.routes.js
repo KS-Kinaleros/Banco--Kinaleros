06c5ebc84a6fd34549b49fe41ed3da8e4c3ed988
dperez <dperez-2018546@kinal.edu.gt>
2023-06-09 04:16:16 -0600

    CRUD de deposit

---
+++ Banco-Back-End/src/deposit/deposit.routes.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const express = require('express')
+const api = express.Router()
+const depositController = require('./deposit.controller')
+const { ensureAuth, isAdmin } = require('../services/authenticated')
+
+api.get('/test', depositController.test)
+api.post('/save', ensureAuth, depositController.save)
+api.put('/update/:id', depositController.update)
+api.delete('/cancel/:id', depositController.cancel)
+api.get('/get', depositController.get)
+
+module.exports = api
